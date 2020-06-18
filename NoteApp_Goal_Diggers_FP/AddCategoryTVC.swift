44b027836915572ff17188595671f8d7c62995a5
gagandeep <sherry2312.gk@gmail.com>
2020-06-18 17:37:07 -0400

    [gagan] adding bar button for adding notes

--- NoteApp_Goal_Diggers_FP/AddCategoryTVC.swift
+++ NoteApp_Goal_Diggers_FP/AddCategoryTVC.swift
@@ -43,6 +43,11 @@ class AddCategoryTVC: UITableViewController {
     }
     */
 
+    
+    
+    @IBAction func AddNotes(_ sender: UIBarButtonItem) {
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
