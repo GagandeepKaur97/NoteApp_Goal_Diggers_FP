63b462b5bca913bf429bc10d12cde720ce28b831
gagandeep <sherry2312.gk@gmail.com>
2020-06-20 16:18:32 -0400

    [gk] adding code for date location

--- NoteApp_Goal_Diggers_FP/NotesCell.swift
+++ NoteApp_Goal_Diggers_FP/NotesCell.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-
+import CoreData
+import CoreLocation
 class NotesCell: UITableViewCell {
 
     
@@ -29,5 +30,50 @@ class NotesCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+      func setData(note: NSManagedObject){
+            title.text = note.value(forKey: "title") as! String
+            
+            //get date
+            let dateFormat = DateFormatter()
+            dateFormat.dateFormat = "dd/MM/yyyy"
+            let formattedDate = dateFormat.string(from: note.value(forKey: "dateTime") as! Date)
+            Date.text = formattedDate
 
-}
+            //get time
+            let timeFormat = DateFormatter()
+            timeFormat.dateFormat = "HH:MM:SS"
+            let formattedTime = timeFormat.string(from: note.value(forKey: "dateTime") as! Date)
+           Time.text = formattedTime
+            
+            getAddress(lat: note.value(forKey: "lat") as! Double, long: note.value(forKey: "long") as! Double)
+        }
+        
+        func getAddress(lat: Double, long: Double){
+            var address = ""
+            CLGeocoder().reverseGeocodeLocation(CLLocation(latitude: lat, longitude: long)) { (placemarks, error) in
+                if let error = error{
+                    print(error)
+                    
+                } else{
+                    
+                    if let placemark = placemarks?[0]{
+                        if placemark.locality != nil{
+                            address += placemark.locality!
+                        }
+                    }
+                    if let placemark = placemarks?[0]{
+                        if placemark.subAdministrativeArea != nil{                        address += ", \(placemark.subAdministrativeArea!), "
+                        }
+                    }
+                    if let placemark = placemarks?[0]{
+                        if placemark.administrativeArea != nil{
+                            address += placemark.administrativeArea!
+                        }
+                    }
+    //                
+                    self.location.text = address
+                }
+            }
+        }
+
+    }
